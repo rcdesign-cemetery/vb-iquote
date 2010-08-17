d72ffed87771c030fa22bc992f394d169d671b74
Jannie <jpatuk@gmail.com>
2010-08-17 19:01:29 +1100

    Code cleanup

--- upload/clientscript/rcd_iquote.js
+++ upload/clientscript/rcd_iquote.js
@@ -19,50 +19,48 @@ var iQuote = {
     context_menu: null,
 
     /**
-     *
+     * Container for PostBit_Init function
      */
-    Pre_Votes_PostBit_Init: null,
+    pre_iquote_postbit_init: null,
 
     /**
      * onload event handler
      *
-     * @param string post_list_id - ID of element which contains all posts
+     * @param string posts_container_id - ID of element which contains all posts
      */
-    init: function(post_list_id) {
-        var post_list = fetch_object(post_list_id);
-        if ( !post_list ) {
+    init: function(posts_container_id) {
+        var posts_container = fetch_object(posts_container_id);
+        if ( !posts_container ) {
             return;
         }
 
-        //make sure quick reply is there. taken from vbulletioin_quick_reply.js
+        // init menu only if quick reply box exists
         if (!fetch_object('quick_reply')) {
            return;
         }
 
-        iQuote.add_handlers(post_list);
-
-        // generate menu which shown even click occurs not on menu
         iQuote.context_menu = new YAHOO.widget.Menu("iquote_popup_menu", {'clicktohide': false});
         iQuote.context_menu.render(document.body);
+        iQuote.add_handlers(posts_container);
+        YAHOO.util.Dom.setStyle(YAHOO.util.Dom.getElementsByClassName("popupbody", "*", fetch_object('iquote_popup_menu')), "display", "block");
 
-        // override PostBit_Init
-        Pre_Votes_PostBit_Init = PostBit_Init;
+        // init for AJAX loaded posts (inline edit etc)
+        iQuote.pre_iquote_postbit_init = PostBit_Init;
         PostBit_Init = function (obj, post_id)
         {
             iQuote.add_handlers(obj);
 
-            Pre_Votes_PostBit_Init(obj, post_id);
+            iQuote.pre_iquote_postbit_init(obj, post_id);
         }
-
     },
 
     /**
      * add handler on posts
      *
-     * @param string post_list - object fetched by post_list_id
+     * @param string posts_container - object fetched by posts_container_id
      */
-    add_handlers: function(post_list) {
-        var post_rows = YAHOO.util.Dom.getElementsByClassName("postbody","*",post_list);
+    add_handlers: function(posts_container) {
+        var post_rows = YAHOO.util.Dom.getElementsByClassName("postbody", "*", posts_container);
         var check_for_id = function(el) {
            if ( el.id && (0 <= el.id.indexOf("post_message_")) ) {
               return true;
@@ -71,16 +69,15 @@ var iQuote = {
         };
 
         for ( var i = 0; i < post_rows.length; i++ ) {
-            var post_content = YAHOO.util.Dom.getElementBy(check_for_id,"div",post_rows[i]);
+            var post_content = YAHOO.util.Dom.getElementBy(check_for_id, "div", post_rows[i]);
             if (!post_content || !post_content.id) {
                continue;
             }
             var post_id = post_content.id;
+            var post_number = post_id.substr(post_id.lastIndexOf("_") + 1);
 
-	    var post_number = post_id.substr(post_id.lastIndexOf("_") + 1);
-            
-	    YAHOO.util.Event.on(post_rows[i], "mouseup", iQuote.selectionEnd, post_number);
-	    YAHOO.util.Event.on(post_rows[i], "mousedown", iQuote.selectionStart, post_number);
+            YAHOO.util.Event.on(post_rows[i], "mouseup", iQuote.selectionEnd, post_number);
+            YAHOO.util.Event.on(post_rows[i], "mousedown", iQuote.selectionStart, post_number);
         }
     },
 
@@ -107,13 +104,11 @@ var iQuote = {
             }
         }
 
-        // hide menu w/o scroll to prev. selected element if it is shown
         if (iQuote.context_menu)
         {
             iQuote.hide_menu();
         }
 
-        // selection must be empty at this step
         iQuote.selected_post_number = post_number;
     },
 
@@ -181,19 +176,11 @@ var iQuote = {
                     }
                 }
 
-                //iquote widget is based on standard popup class, which display property is set to none by default
-                YAHOO.util.Dom.setStyle(YAHOO.util.Dom.getElementsByClassName("popupbody","*",fetch_object('iquote_popup_menu')), "display", "block");
                 iQuote.context_menu.show();
 
-                // hide menu for prevent adding of text content of menu to selected text during copypasting by context menu or CTRL+C/CTRL+V
-                var copy_callback = function(e)
-                {
-                    iQuote.hide_menu();
-                    return true;
-                }
-
-                YAHOO.util.Event.on(document, "keydown", copy_callback);
-                YAHOO.util.Event.on(document, "contextmenu", copy_callback);
+                // hide menu to prevent adding of text content of menu to selected text during copypasting by context menu or CTRL+C/CTRL+V
+                YAHOO.util.Event.on(document, "keydown", iQuote.hide_menu);
+                YAHOO.util.Event.on(document, "contextmenu", iQuote.hide_menu);
             } else {
                iQuote.selected_post_number = 0;
             }
@@ -238,35 +225,24 @@ var iQuote = {
             return false;
         }
 
-        var s = iQuote.getScrollXY();
-
-        //store previously entered text, as qr_handle_activate will clear it
+        // store previously entered text, as qr_handle_activate will clear it
         var previously_entered_comment = vB_Editor[QR_EditorID].get_editor_contents();
         var quote_text = '';
 
-        // ff even on empty text returns <br>, so we'll won't insert it to avoid empty leading string
+        // ff even on empty text returns <br>, so we won't insert it to avoid empty leading string
         if (previously_entered_comment && previously_entered_comment != "<br>") {
            quote_text += previously_entered_comment;
         }
-                                                                                                     
-        //reposition Quick Reply form
+
         iQuote.repositionQuickReply(iQuote.selected_post_number);
 
         var username = iQuote.getPosterName(iQuote.selected_post_number);
-
-        //show hidden quick reply after it was moved
-        qr_activate(iQuote.selected_post_number);
-
-        // from vB_Text_Editor_Events.prototype.attachinsertall_onclick (file clientscript/vbulletin_textedit.js)
         var breakchar = (vB_Editor[QR_EditorID].wysiwyg_mode ? '<br /><br />' : '\r\n\r\n');
         quote_text += "[quote=" + username + ";" + iQuote.selected_post_number + "]" + iQuote.selected_text + "[/quote]" + breakchar;
-        
         vB_Editor[QR_EditorID].insert_text(quote_text, quote_text.length, 0);
 
         iQuote.selected_post_number = 0;
         iQuote.selected_text = '';
-
-        // hide menu w/o scrolling to prev. selected element
         iQuote.hide_menu();
 
         return false;
@@ -280,28 +256,29 @@ var iQuote = {
      */
     repositionQuickReply: function(post_id)
     {
-        // make the cancel button visible
-	var cancelbtn = fetch_object('qr_cancelbutton');
-	cancelbtn.style.display = '';
+        var cancelbtn = fetch_object('qr_cancelbutton');
+        cancelbtn.style.display = '';
 
         // add form into container below the post we are replying to
-	var qrobj = document.createElement("li");
-	qrobj.id = "qr_" + post_id;
-	var post = YAHOO.util.Dom.get("post_" + post_id);
-	var qr_container = post.parentNode.insertBefore(qrobj, post.nextSibling);
-	var qr_form = fetch_object('quick_reply');
-	qr_container.appendChild(qr_form);
-
+        var qrobj = document.createElement("li");
+        qrobj.id = "qr_" + post_id;
+        var post = YAHOO.util.Dom.get("post_" + post_id);
+        var qr_container = post.parentNode.insertBefore(qrobj, post.nextSibling);
+        var qr_form = fetch_object('quick_reply');
+        qr_container.appendChild(qr_form);
+        qr_activate(iQuote.selected_post_number);
     },
 
     /**
      * get name of poster
+     * @param id of post which author we want to know
      */
     getPosterName: function(post_number)
     {
         var post = document.getElementById("post_" + post_number);
-        var username = YAHOO.util.Dom.getElementsByClassName( "username", "a", post )[0];
-        username = username.text || username.textContent || username.innerText || username.innerHTML.replace(/<[^>]+>/g, "");
+        var username = YAHOO.util.Dom.getElementsByClassName( "username", "*", post )[0];
+        username = username.textContent || username.innerText;
+        
         return PHP.htmlspecialchars(username);
     },
 
@@ -311,44 +288,10 @@ var iQuote = {
      * used as fix for problems with copypaste (content of menu is added to selected text) and menu hide (page is scrolled to prev. selected element)
      */
     hide_menu: function() {
-        var clone = iQuote.context_menu.srcElement.cloneNode(true);
-        clone.id = "iquote_popup_menu";
+        iQuote.context_menu.hide();
 
-        iQuote.context_menu.destroy();
-
-        iQuote.context_menu = new YAHOO.widget.Menu(clone, {'clicktohide': false});
-        iQuote.context_menu.render(document.body);
-    },
-
-    /**
-     * get current scroll position
-     */
-    getScrollXY: function()
-    {
-        var scrOfX = 0, scrOfY = 0;
-
-        if (typeof( window.pageYOffset ) == 'number') // Netscape compliant
-        {
-            scrOfY = window.pageYOffset;
-            scrOfX = window.pageXOffset;
-        }
-        else if (document.body && ( document.body.scrollLeft || document.body.scrollTop )) // DOM compliant
-        {
-            scrOfY = document.body.scrollTop;
-            scrOfX = document.body.scrollLeft;
-        }
-        else if (document.documentElement && ( document.documentElement.scrollLeft || document.documentElement.scrollTop )) // IE6 standards compliant mode
-        {
-            scrOfY = document.documentElement.scrollTop;
-            scrOfX = document.documentElement.scrollLeft;
-        }
-
-        if ( typeof( window.pageYOffset ) != 'number' && !document.body && !document.documentElement ) // Unsupported browser
-        {
-            return false;
-        }
-        
-        return [ scrOfX, scrOfY ];
+        YAHOO.util.Event.removeListener(document, "keydown", iQuote.hide_menu);
+        YAHOO.util.Event.removeListener(document, "contextmenu", iQuote.hide_menu);
+        return true;
     }
-
 };
